10004e5c1ee9aefe3a323b44fcc157a98e31a322
vassik <anatoly.vasilevskiy@sintef.no>
2018-07-18 14:40:46 +0200

    deploying experiment

--- deploy_exp.sh
+++ deploy_exp.sh
@@ -17,18 +17,19 @@ echo "Copying SUT"
 rm -rf ./$SUT
 cp -R ../$SUT .
 
-rm -rf $HOME/$SHARED
-#docker stop $APP
-#docker rm $APP
-#docker rmi -f $IMAGE
-#docker build --rm -t $IMAGE .
+docker rmi -f $IMAGE
+docker build --rm -t $IMAGE .
 
 NUMBER_OF_CONFIGS=$1
 
-for (( i = 0; i < 1; i++ )); do
+for (( i = 0; i < 10; i++ )); do
+	echo "Cleaning up..."
+	rm -rf $HOME/$SHARED
+	docker stop $APP
+	docker rm $APP
+
 	START=$(date +%s.%N)
-	#docker run -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/$SHARED:/var/jenkins_home/report/ -e MASTER_SSH_PORT=22 -e MASTER_SLAVE_USER=jenkins -e MASTER_SLAVE_PWD=jenkins -e DOCKER_GID=$DOCKERGID --name $APP $IMAGE
-	sleep 1
+	docker run -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/$SHARED:/var/jenkins_home/report/ -e MASTER_SSH_PORT=22 -e MASTER_SLAVE_USER=jenkins -e MASTER_SLAVE_PWD=jenkins -e DOCKER_GID=$DOCKERGID --name $APP $IMAGE
 	END=$(date +%s.%N)
 	DIFF=$(echo "$END - $START" | bc)
 	echo "Tested $NUMBER_OF_CONFIGS configs in $DIFF sec" | tee -a $TIME_MEASURED/output_${i}
