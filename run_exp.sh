bb5a7e841b9e1786392df4c797ad25308aba2b1a
vassik <anatoly.vasilevskiy@sintef.no>
2018-07-18 14:26:07 +0200

    launching wrong script, fixed

--- run_exp.sh
+++ run_exp.sh
@@ -5,7 +5,7 @@ for (( i = 0; i < 5; i++ )); do
 	echo "Copying files"
 	cp experiments/config_${i}.ini testframework/config.ini
 	for (( j = 0; j < 5; j++ )); do
-		./deploy.sh $j
+		./deploy_exp.sh $j
 	done
 done
 echo "All experiments are completed!"
